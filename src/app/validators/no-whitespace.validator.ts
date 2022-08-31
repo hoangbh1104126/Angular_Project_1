73b2bfd3611cd2bc828ac8fcb896f7fac379212e
hoangbh1104126 <110478946+hoangbh1104126@users.noreply.github.com>
2022-08-31 13:03:35 +0700

    Update 2

--- src/app/validators/no-whitespace.validator.ts
+++ src/app/validators/no-whitespace.validator.ts
@@ -1,1 +1,14 @@
+import { AbstractControl, ValidatorFn } from "@angular/forms";
+
+export function NoWhitespaceValidator(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } => {
+    let controlVal = control.value;
+    if (typeof controlVal === "number") {
+      controlVal = `${controlVal}`;
+    }
+    let isWhitespace = (controlVal || "").trim().length === 0;
+    let isValid = !isWhitespace;
+    return isValid ? {} : { whitespace: "value is only whitespace" };
+  };
+}
 
